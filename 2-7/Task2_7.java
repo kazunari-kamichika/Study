d497128b09946e88fd86b67d497d23107007b1c5
kazunari-kamichika <155711273+kazunari-kamichika@users.noreply.github.com>
2024-01-14 01:10:54 +0900

    2-7

---
+++ 2-7/Task2_7.java
@@ -0,0 +1,39 @@
+public class Task2_7 {
+
+    public static void main(String[] args) {
+        
+        //問1
+        String[] country = {"JAPAN","AMERIKA","KOREA","ENGLAND"}; 
+
+        //問2
+        System.out.println(country.length);
+
+        //問3
+        String[] strArray = new String[3];
+        strArray[0] = "りんご";
+        strArray[1] = "もも";
+        strArray[2] = "ぶどう";
+
+        //問4
+        System.out.println(strArray[2]);
+
+        //問5
+        //[int[]型のは配列変数intArrayの宣言をして10, 20, 30, 40, 50で初期化している]
+
+        int[] intArray = { 10, 20, 30, 40, 50 };
+
+        //問6
+
+        //[配列変数intArrayの1番目の要素と5番目の要素を足した要素を出力する]
+
+        System.out.println(intArray[0] + intArray[4]);
+
+
+
+
+
+
+
+        
+    }
+}
