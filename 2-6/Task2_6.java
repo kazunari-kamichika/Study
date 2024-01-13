864c23ac866dccbf0f8fc7b34d8a7336f238dce5
kazunari-kamichika <155711273+kazunari-kamichika@users.noreply.github.com>
2024-01-13 12:34:26 +0900

    2-6

---
+++ 2-6/Task2_6.java
@@ -0,0 +1,29 @@
+public class Task2_6{
+
+    public static void main(String[] args){
+      //問1
+     int hogeInt =50;
+      //問
+     String hogeString ="テスト";
+ 
+      //問3
+      //[doubleの型でhogeDoubleという変数で宣言して、値「0.8」で初期化している]
+      double hogeDouble = 0.8;
+
+      //問4
+       hogeInt  = 100;
+
+      //問5
+       hogeString = "良くできました！";
+
+      //問6
+      System.out.println(hogeInt);
+
+      //問7
+      //[("")中のに書いた文字列が出力される]
+       System.out.println(hogeString);
+       
+       //問8
+       System.out.println(hogeDouble);
+    }
+}
