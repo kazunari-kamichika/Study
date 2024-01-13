9ecb6b94897792928994c62657f464924f497576
kazunari-kamichika <155711273+kazunari-kamichika@users.noreply.github.com>
2024-01-14 02:33:17 +0900

    2-8

---
+++ 2-8/Task2_8.java
@@ -0,0 +1,53 @@
+public class Task2_8 {
+
+    public static void main(String[] args) {
+
+        //問1
+        int plus = 5 + 16;
+
+        //問2
+        int minus = 27 - 7;
+
+        //問3
+        //[10と2を乗算する]
+        int multiply = 10 * 2;
+
+        //問4
+        int remi =20;
+          remi %=6;
+
+        //問5
+        // [stringの型にtownとlineという名前でそれぞれ"中央"と"線"で初期化している]
+
+        String town = "中央";
+        String line = "線";
+
+        //問6
+        String train = town + line;
+
+        //問7
+
+        System.out.println(plus);
+
+        System.out.println(minus);
+
+        System.out.println(multiply);
+
+        System.out.println(remi);
+
+        System.out.println(train);
+
+
+
+
+
+
+
+
+
+        
+
+
+
+    }
+}
