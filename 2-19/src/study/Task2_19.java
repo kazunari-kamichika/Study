0489e8f55231c4b2760fe7bcd625acecfdb49fcb
kazunari-kamichika <155711273+kazunari-kamichika@users.noreply.github.com>
2024-01-26 22:38:19 +0900

    2-19

---
+++ 2-19/src/study/Task2_19.java
@@ -0,0 +1,19 @@
+package study;
+
+import constants.FruitsConstants;
+import fruits.Fruits;
+
+public class Task2_19 {
+	
+	public static void main(String[] args) {
+		
+		
+		Fruits.printFruits(FruitsConstants.FRUITS_LEMON_04,FruitsConstants.FRUITS_COUNT_20);
+		 
+
+	
+  }
+		
+}
+
+
