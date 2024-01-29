21f587267cd0a5e77c767304ec7d15e1b5038f3f
kazunari-kamichika <155711273+kazunari-kamichika@users.noreply.github.com>
2024-01-30 00:46:28 +0900

    2-20

---
+++ 2-20/src/study/Child.java
@@ -0,0 +1,28 @@
+package study;
+
+ //問①
+ class Child extends Parent{
+	 
+	 
+	//問② 
+	 public void callName() {
+		 
+		 super.name();
+		 
+	 }
+	
+	
+	//問③
+	 public void update() {
+		
+		System.out.println("上書きされました。");
+	}
+	 
+ }
+
+
+
+ 
+ 
+ 
+ 
---
+++ 2-20/src/study/Task2_20.java
@@ -0,0 +1,30 @@
+package study;
+
+public class Task2_20 {
+	
+	public static void main(String[] args) {
+		
+		//問④
+		Child child = new Child();
+		
+		//問⑤
+		child.callName();
+		
+		//問⑥
+		child.update();
+		
+		//問⑦
+		
+		Parent number = new Parent();
+				
+			   number.argument();
+			
+			   number.argument(1);
+			
+			   number.argument(1,1);
+		
+		
+		
+	}
+
+}
